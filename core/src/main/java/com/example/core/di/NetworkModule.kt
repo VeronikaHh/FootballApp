9bb2b4a74e1b28a4307b8afcae5a168962671c6f
VeronikaHh <golovanovaveronika@gmail.com>
2022-10-26 12:42:54 +0300

    retrofit

---
+++ core/src/main/java/com/example/core/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.example.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private val baseUrl : String = "https://api-football-v1.p.rapidapi.com/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(BASE_URL : String) : Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+}
