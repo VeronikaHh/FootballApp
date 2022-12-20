c43860d3c3790919402ec56933335e79162e777b
Oleh <oleh.kononenko@newsoft-ns.com>
2022-12-20 11:12:46 +0200

    handle deep links refactoring

--- app/src/main/java/com/example/holovanova_football/MainActivity.kt
+++ app/src/main/java/com/example/holovanova_football/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.holovanova_football
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,24 +12,30 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Football)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        bottomNavigationView.itemIconTintList = null
-        bottomNavigationView.setupWithNavController(navController)
+        setupNavigation()
+        setupBottomNavigation()
+        navController.handleDeepLink(intent)
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
         navController.handleDeepLink(intent)
+    }
+
+    private fun setupNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+    }
+
+    private fun setupBottomNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.itemIconTintList = null
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
